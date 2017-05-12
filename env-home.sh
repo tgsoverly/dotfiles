ff643da6633b27323ac0e2b363be25d54e6477f9
Tim <timothy@overly.me>
2017-05-12 07:11:39 -0400

    update script

---
+++ env-home.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# Specfic aliases and commands for my personal environments
+alias evo="cd ${CODE_DIR}/evo"
+alias ec="cd ${CODE_DIR}/evo/grails-app/assets/client"
+alias mm="cd ${CODE_DIR}/motmot"
+
+# Computers
+alias pi1='ssh pi@10.0.1.31'
+alias pi2='ssh pi@10.0.1.32'
+alias firefly='ssh root@198.199.64.196'
+alias motmot='ssh root@45.55.178.30'
+alias evospace='ssh -L 3306:localhose:3306 root@104.131.86.143'
---
+++ env-work.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# Specfic aliases and commands for my SPIDA environments
+export ARTIFACTORY_USERNAME="developer"
+export ARTIFACTORY_URL_BASE="https://dev.spidasoftware.com/artifactory"
+export ARTIFACTORY_URL="${ARTIFACTORY_URL_BASE}/maven-repo"
+export ARTIFACTORY_URL_EXPOSED="${ARTIFACTORY_URL_BASE}/exposed-repo"
