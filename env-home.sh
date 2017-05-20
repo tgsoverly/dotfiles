d0aaaedfd787c18a701a3658173f44149046b27e
Tim <timothy@overly.me>
2017-05-20 19:04:58 -0400

    automated commit bash-profile to keep computers in sync

--- env-home.sh
+++ env-home.sh
@@ -8,7 +8,7 @@ alias mm="cd ${CODE_DIR}/motmot"
 # Computers
 alias pi1='ssh pi@10.0.1.31'
 alias pi2='ssh pi@10.0.1.32'
-alias firefly='ssh root@198.199.64.196'
+alias firefly='ssh root@104.131.165.120'
 alias motmot='ssh root@45.55.178.30'
 alias evospace='ssh -L 3306:localhose:3306 root@104.131.86.143'
 
